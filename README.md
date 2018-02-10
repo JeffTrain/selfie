5360ece31e6395dfe6ea85d0b84859664a846b92
Jefåf Tian <jeff.tian@outlook.com>
2018-02-10 11:30:11 +0800

    识别人脸的68个特征点，并用红点圈出。

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # selfie
 可以双击运行的桌面程序，在自拍时给人脸添加酷酷的特效。
+教学专用，你可以跟着代码提交历史，一步步实现很酷的功能
+
+教学[PPT](自拍坊.pptx)
+
+# 关键步骤
+* 人脸识别并跟踪（仅需41行代码，效果如下）：
+![在马桶上就完成了](toilet.png)
+
+* 加特效 (TODO)
+
+# 本地开发
+```bash
+git clone https://github.com/JeffTrain/selfie.git
+cd selfie
+pip install -r requirements.txt
+```
+
+# 本地运行
+```bash
+cd selfie
+python app.py
+```
--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import cv2
 import dlib
+import numpy as np
+
+import os
+import os.path
 
 video_capture = cv2.VideoCapture(0)
 face_detector = dlib.get_frontal_face_detector()
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+landmark_file_path = os.path.realpath(os.path.join(dir_path, './shape_predictor_68_face_landmarks.dat'))
+predictor = dlib.shape_predictor(landmark_file_path)
 
 
 def rect_to_bounding_box(rect):
@@ -10,12 +18,27 @@ def rect_to_bounding_box(rect):
     y = rect.top()
     w = rect.right() - x
     h = rect.bottom() - y
-    return (x, y, w, h)
+    return x, y, w, h
 
 
 def scale_rect(rect, scale):
     (x, y, w, h) = rect
-    return (int(x / scale), int(y / scale), int(w / scale), int(h / scale))
+    return int(x / scale), int(y / scale), int(w / scale), int(h / scale)
+
+
+def shape_to_np(shape, dtype="int"):
+    coords = np.zeros((68, 2), dtype=dtype)
+
+    for i in range(0, 68):
+        coords[i] = (shape.part(i).x, shape.part(i).y)
+
+    return coords
+
+
+def scale_point(point, scale):
+    (x, y) = point
+    return int(x / scale), int(y / scale)
+
 
 def trace_face(frame):
     scale = 200 / min(frame.shape[1], frame.shape[0])
@@ -24,10 +47,13 @@ def trace_face(frame):
     faces = face_detector(gray, 1)
 
     for i, face_rect in enumerate(faces):
-        (x, y, w, h) = scale_rect(rect_to_bounding_box(face_rect), scale)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2) 
+        shape = predictor(gray, face_rect)
+        shape = shape_to_np(shape)
+        for point in shape:
+            cv2.circle(frame, scale_point(point, scale), 2, (0, 0, 255), 1)
 
     return frame
+
 
 while True:
     ret, frame = video_capture.read()
@@ -39,4 +65,3 @@ while True:
 
 video_capture.release()
 cv2.destroyAllWindows()
-
